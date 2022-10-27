e7ed5bc74d850a2a9bbc986b8c100a3e32bf00ac
vieiragah <107413566+vieiragah@users.noreply.github.com>
2022-10-27 10:15:31 -0300

    organograma

---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,23 @@
+import './CampoTexto.css'
+const CampoTexto = (props) => {
+
+
+
+    const aoDigitado = (event) => {
+        props.aoAlterado(event.target.value)
+    }
+
+    return (
+        <div className="campo_texto">
+            <label>{props.label}</label>
+            <input
+            value = {props.valor}
+            onChange={aoDigitado} 
+            required={props.obrigatorio} 
+            placeholder={props.placeholder} 
+            />
+        </div>
+    )
+
+}
+export default CampoTexto
